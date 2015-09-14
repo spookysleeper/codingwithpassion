31cccb2074efa810f7cc703ffbae0846d69bc073
spookysleeper <spookysleeper@gmail.com>
2015-09-14 15:59:19 +0200

    playing...

--- leaks/src/main/java/org/cwp/TestLeak.java
+++ leaks/src/main/java/org/cwp/TestLeak.java
@@ -1,9 +1,20 @@
 package org.cwp;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestLeak {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+        List leak = new ArrayList();
+
 		System.out.println("Run test...");
+		while(true) {
+            Thread.sleep(100);
+            leak.add(new ArrayList<BigDecimal>(1000));
+		}
 	}
 
 }
