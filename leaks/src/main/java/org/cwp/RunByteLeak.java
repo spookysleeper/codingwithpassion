26fb82e041fe42d9e7ba97fb334f9a28e5767f8d
spookysleeper <spookysleeper@gmail.com>
2015-09-18 09:50:31 +0200

    add new leak - HashMap

--- leaks/src/main/java/org/cwp/RunByteLeak.java
+++ leaks/src/main/java/org/cwp/RunByteLeak.java
@@ -6,7 +6,11 @@ import java.util.List;
 //Simple heap leaks
 //Refs: http://stackoverflow.com/questions/4062512/a-simple-program-to-demonstrate-memory-leak-in-java
 //http://www.codeproject.com/Articles/30593/Effective-Java
+//Primitive types sizes: http://btoddb-java-sizing.blogspot.sk/2012/01/object-sizes.html
+//Class-loader leaks
 public class RunByteLeak {
+
+    private static final int ONE_MEGABYTE = 1024 * 1024;
 
 	public static void main(String[] args) throws Exception {
         System.out.println("Run byte leak...");
@@ -16,8 +20,9 @@ public class RunByteLeak {
     private static void runByteTestLeak() throws InterruptedException {
         List listOfBytes = new ArrayList();
         while(true) {
-            byte[] b = new byte[1024*1024];
-            listOfBytes.add(b);
+            //http://stackoverflow.com/questions/24559839/memory-usage-of-byte-array-in-java
+            byte[] mb = new byte[ONE_MEGABYTE];
+            listOfBytes.add(mb);
             Thread.sleep(100);
         }
     }
