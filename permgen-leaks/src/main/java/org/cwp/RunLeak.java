3380411019d1d83c321dfdd83c5eb0a33d003108
spookysleeper <spookysleeper@gmail.com>
2015-10-09 09:52:25 +0200

    basic test run

--- permgen-leaks/src/main/java/org/cwp/RunLeak.java
+++ permgen-leaks/src/main/java/org/cwp/RunLeak.java
@@ -3,10 +3,7 @@ package org.cwp;
 public class RunLeak {
 
     public static void main(String [] args) throws Exception {
-        String className = args[0];
-        Class leakClass = Class.forName("org.cwp." + className);
-        Leak leak = (Leak)leakClass.newInstance();
-        leak.run(args);
+        System.out.println("Yes");
     }
 
 }
