37b69370b26eddf748f42d36b018da6d6ad171b5
spooky <spookysleeper@gmail.com>
2018-03-27 08:36:05 +0200

    remove scala

--- scala/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "scala"
-
-version := "0.1"
-
-scalaVersion := "2.12.4"
-        
--- scala/src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-
-object Main {
-
-    def main(args: Array[String]): Unit = {
-        println("Hello from main of class")
-    }
-
-}
